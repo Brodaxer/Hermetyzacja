b14a9803eede3336e088cda727c5491ab1bd6757
Brodaxer <mloszewski123@gmail.com>
2023-01-25 13:35:40 +0100

    Initial commit

---
+++ src/printInfo.java
@@ -0,0 +1,11 @@
+class printInfo {
+
+
+}
+//        System.out.println("Witaj " + client1.getFirstName() + " " + client1.getLastName());
+//        System.out.println("Kwota przed rabatem: " + price1);
+//        System.out.println("Do zapłaty (po rabacie): " + priceDiscount1);
+//
+//        System.out.println("Witaj " + client2.getFirstName() + " " + client2.getLastName());
+//        System.out.println("Kwota przed rabatem: " + price2);
+//        System.out.println("Do zapłaty (po rabacie): " + priceDiscount2);
